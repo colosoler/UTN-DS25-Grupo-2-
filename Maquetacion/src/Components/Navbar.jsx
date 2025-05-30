9a7fcf24d503ee3ff470159755b05058a572f0ba
Ramiro Gil <gilramiro49@gmail.com>
2025-05-30 16:42:42 -0300

    Agrego los componentes de navbar y searchbar

--- Maquetacion/src/Components/Navbar.jsx
+++ Maquetacion/src/Components/Navbar.jsx
@@ -1,7 +1,18 @@
-
+import './styles/Navbar.css';
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <span>📝<strong>UTN</strong>otas</span>
+      </div>
+      <div className="navbar-icons">
+        <img src="./images/hogar.png" alt="Inicio"/>
+        <img src="./images/plus-pequeno.png" alt="Agregar contenido"/>
+        <img src="./images/avatar.png" alt="Perfil"/>
+      </div>
+    </nav>
   )
 }
+
+
--- Maquetacion/src/Components/Searchbar.jsx
+++ Maquetacion/src/Components/Searchbar.jsx
@@ -1,5 +1,23 @@
+import './styles/Searchbar.css';
+
 export const Searchbar = () => {
   return (
-    <div>Searchbar</div>
-  )
+
+    <div className="searchbar-container">
+      <label className="search-label"></label>
+      <div className="search-wrapper">
+        <input
+          type="text"
+          placeholder="Ingresa un tema o una materia"
+          className="search-input"
+        />
+        <button className="clear-btn" title="Borrar">×</button>
+        <button className="menu-btn" title="Menú">≡</button>
+      </div>
+    </div>
+  );
 }
+
+
+
+
