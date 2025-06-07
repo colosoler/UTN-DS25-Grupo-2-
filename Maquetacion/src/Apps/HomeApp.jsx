ee9865b939ad1d86e849ba0a0f9adc625dc5888b
Franco Arce <165492758+FrancoArcee@users.noreply.github.com>
2025-06-07 10:05:55 -0300

    Modificamos Login y aplicamos estilos al Home y Carreras

--- Maquetacion/src/Apps/HomeApp.jsx
+++ Maquetacion/src/Apps/HomeApp.jsx
@@ -4,28 +4,28 @@ import './styles/HomeApp.css';
 
 const carreras = [
   { id: 1, 
-    name: 'Ingeniería en Sistemas', 
-    image:'https://www.google.com/search?sca_esv=3daf0913dc600547&rlz=1C1GCEU_esAR1161AR1161&sxsrf=AE3TifPIjVujl5wD8ts6EvxnASzqKABb1A:1748469733793&q=ingenieria+en+sistemas+icono&udm=2&fbs=AIIjpHx4nJjfGojPVHhEACUHPiMQht6_BFq6vBIoFFRK7qchKL_ojr3cM9VEz3-sGXDgSIVKY0s5WrJRJaM2g4iCN_WZWgD9NdXmHSHVKNntviVhJf_Of57WHiY3Hi-IBSD5JeOLl5K3ATQBZmLFjun0cm2Ga3B3V0tmrVrgtg5JemoSx8oZEifK_kcew5u9yltg7m9q60gRxIrHNS9ixS--MQCm6yI46Q&sa=X&ved=2ahUKEwj8oYirlceNAxU3npUCHcZJMo8QtKgLegQIEhAB&biw=1366&bih=607&dpr=1#vhid=njajZcanJQDf9M&vssid=mosaic'
+    name: 'Ingeniería Mecánica', 
+    image:'/images/Mecanica.jpg'
   },
   { id: 2, 
-    name: 'Ingeniería en Sistemas', 
-    image:'https://www.google.com/search?sca_esv=3daf0913dc600547&rlz=1C1GCEU_esAR1161AR1161&sxsrf=AE3TifPIjVujl5wD8ts6EvxnASzqKABb1A:1748469733793&q=ingenieria+en+sistemas+icono&udm=2&fbs=AIIjpHx4nJjfGojPVHhEACUHPiMQht6_BFq6vBIoFFRK7qchKL_ojr3cM9VEz3-sGXDgSIVKY0s5WrJRJaM2g4iCN_WZWgD9NdXmHSHVKNntviVhJf_Of57WHiY3Hi-IBSD5JeOLl5K3ATQBZmLFjun0cm2Ga3B3V0tmrVrgtg5JemoSx8oZEifK_kcew5u9yltg7m9q60gRxIrHNS9ixS--MQCm6yI46Q&sa=X&ved=2ahUKEwj8oYirlceNAxU3npUCHcZJMo8QtKgLegQIEhAB&biw=1366&bih=607&dpr=1#vhid=njajZcanJQDf9M&vssid=mosaic'
+    name: 'Ingeniería en Sistemas de Información', 
+    image:'/images/Sistemas.jpg'
   },
   { id: 3, 
-    name: 'Ingeniería en Sistemas', 
-    image:'https://www.google.com/search?sca_esv=3daf0913dc600547&rlz=1C1GCEU_esAR1161AR1161&sxsrf=AE3TifPIjVujl5wD8ts6EvxnASzqKABb1A:1748469733793&q=ingenieria+en+sistemas+icono&udm=2&fbs=AIIjpHx4nJjfGojPVHhEACUHPiMQht6_BFq6vBIoFFRK7qchKL_ojr3cM9VEz3-sGXDgSIVKY0s5WrJRJaM2g4iCN_WZWgD9NdXmHSHVKNntviVhJf_Of57WHiY3Hi-IBSD5JeOLl5K3ATQBZmLFjun0cm2Ga3B3V0tmrVrgtg5JemoSx8oZEifK_kcew5u9yltg7m9q60gRxIrHNS9ixS--MQCm6yI46Q&sa=X&ved=2ahUKEwj8oYirlceNAxU3npUCHcZJMo8QtKgLegQIEhAB&biw=1366&bih=607&dpr=1#vhid=njajZcanJQDf9M&vssid=mosaic'
+    name: 'Ingeniería Química', 
+    image:'/images/Quimica.jpg'
   },
   { id: 4, 
-    name: 'Ingeniería en Sistemas', 
-    image:'https://www.google.com/search?sca_esv=3daf0913dc600547&rlz=1C1GCEU_esAR1161AR1161&sxsrf=AE3TifPIjVujl5wD8ts6EvxnASzqKABb1A:1748469733793&q=ingenieria+en+sistemas+icono&udm=2&fbs=AIIjpHx4nJjfGojPVHhEACUHPiMQht6_BFq6vBIoFFRK7qchKL_ojr3cM9VEz3-sGXDgSIVKY0s5WrJRJaM2g4iCN_WZWgD9NdXmHSHVKNntviVhJf_Of57WHiY3Hi-IBSD5JeOLl5K3ATQBZmLFjun0cm2Ga3B3V0tmrVrgtg5JemoSx8oZEifK_kcew5u9yltg7m9q60gRxIrHNS9ixS--MQCm6yI46Q&sa=X&ved=2ahUKEwj8oYirlceNAxU3npUCHcZJMo8QtKgLegQIEhAB&biw=1366&bih=607&dpr=1#vhid=njajZcanJQDf9M&vssid=mosaic'
+    name: 'Ingeniería Civil', 
+    image:'/images/Civil.jpg'
   },
   { id: 5, 
-    name: 'Ingeniería en Sistemas', 
-    image:'https://www.google.com/search?sca_esv=3daf0913dc600547&rlz=1C1GCEU_esAR1161AR1161&sxsrf=AE3TifPIjVujl5wD8ts6EvxnASzqKABb1A:1748469733793&q=ingenieria+en+sistemas+icono&udm=2&fbs=AIIjpHx4nJjfGojPVHhEACUHPiMQht6_BFq6vBIoFFRK7qchKL_ojr3cM9VEz3-sGXDgSIVKY0s5WrJRJaM2g4iCN_WZWgD9NdXmHSHVKNntviVhJf_Of57WHiY3Hi-IBSD5JeOLl5K3ATQBZmLFjun0cm2Ga3B3V0tmrVrgtg5JemoSx8oZEifK_kcew5u9yltg7m9q60gRxIrHNS9ixS--MQCm6yI46Q&sa=X&ved=2ahUKEwj8oYirlceNAxU3npUCHcZJMo8QtKgLegQIEhAB&biw=1366&bih=607&dpr=1#vhid=njajZcanJQDf9M&vssid=mosaic'
+    name: 'Ingeniería Industrial', 
+    image:'/images/Industrial.jpg'
   },
   { id: 6, 
-    name: 'Ingeniería en Sistemas', 
-    image:'https://www.google.com/search?sca_esv=3daf0913dc600547&rlz=1C1GCEU_esAR1161AR1161&sxsrf=AE3TifPIjVujl5wD8ts6EvxnASzqKABb1A:1748469733793&q=ingenieria+en+sistemas+icono&udm=2&fbs=AIIjpHx4nJjfGojPVHhEACUHPiMQht6_BFq6vBIoFFRK7qchKL_ojr3cM9VEz3-sGXDgSIVKY0s5WrJRJaM2g4iCN_WZWgD9NdXmHSHVKNntviVhJf_Of57WHiY3Hi-IBSD5JeOLl5K3ATQBZmLFjun0cm2Ga3B3V0tmrVrgtg5JemoSx8oZEifK_kcew5u9yltg7m9q60gRxIrHNS9ixS--MQCm6yI46Q&sa=X&ved=2ahUKEwj8oYirlceNAxU3npUCHcZJMo8QtKgLegQIEhAB&biw=1366&bih=607&dpr=1#vhid=njajZcanJQDf9M&vssid=mosaic'
+    name: 'Ingeniería Eléctrica', 
+    image:'/images/Electrica.jpg'
   },
 ];
 
--- Maquetacion/src/Components/Carrera.jsx
+++ Maquetacion/src/Components/Carrera.jsx
@@ -3,8 +3,8 @@ import './styles/Carrera.css';
 export const Carrera = ({carrera}) => {
   return (
     <div className="carrera">
+      <img src={carrera.image} alt={carrera.name} />
       <h2>{carrera.name}</h2>
     </div>
   )
 }
- 
