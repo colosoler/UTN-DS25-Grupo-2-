1b18e3d1db6ac4b7e839db1056c26cff6af178d0
Ramiro Gil <gilramiro49@gmail.com>
2025-06-12 18:16:00 -0300

    Utilizo Bootstrap para formulario de registro

--- Maquetacion/src/App.jsx
+++ Maquetacion/src/App.jsx
@@ -5,12 +5,13 @@ import { SearchResultApp} from './Apps/SearchResultApp'
 import { Navbar } from './components/Navbar'
 import { Routes, Route, useLocation } from 'react-router-dom'
 import { Login } from './Apps/Login'
+import { Signup } from './Apps/Signup'
 
 function App() {
   const location = useLocation();
 
-  // Con esto oculto el Navbar cuando estamos en Login
-  const hideNavbar = location.pathname === '/';
+  // Con esto oculto el Navbar cuando estamos en Login o en Signup
+  const hideNavbar = location.pathname === '/' || location.pathname === '/signup';
   return (
     <>
 
@@ -19,6 +20,7 @@ function App() {
       <main>
         <Routes>
           <Route path="/" element={<Login/>} />
+          <Route path="/signup" element={<Signup/>} />
           <Route path="/result" element={<SearchResultApp/>} />
           <Route path="/home" element={<HomeApp/>} />
           <Route path="/carrera/:id" element={<CarreraDetailApp/>} />
--- Maquetacion/src/Apps/Signup.jsx
+++ Maquetacion/src/Apps/Signup.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from 'react'
 import { useNavigate, Link } from 'react-router-dom'
 import { Container, Row, Col, Form, Button, Image } from 'react-bootstrap';
-import '../Apps/styles/Login.css'
+import '../Apps/styles/Signup.css'
 
-export const Login = () => {
+export const Signup = () => {
+
+  const [username, setUsername] = useState('')  
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [validated, setValidated] = useState(false)
   const [error, setError] = useState('')
+  const [career, setCareer] = useState('')
   const navigate = useNavigate()
 
   const handleSubmit = (e) => {
@@ -17,19 +20,15 @@ export const Login = () => {
     if (form.checkValidity() === false) {
       e.stopPropagation()
     } else {
-      if (email === 'admin@utn.com' && password === '1234') {
         navigate('/home')
-      } else {
-        setError('Credenciales incorrectas')
-      }
     }
 
     setValidated(true)
   }
 
   return (
-    <div className="login-container">
-      <Form className="login-form" noValidate validated={validated} onSubmit={handleSubmit}>
+    <div className="signup-container">
+      <Form className="signup-form" noValidate validated={validated} onSubmit={handleSubmit}>
         <Container>
           <Row className="justify-content-center mb-3">
             <Col xs={5} md={4}>
@@ -37,8 +36,21 @@ export const Login = () => {
             </Col>
           </Row>
         </Container>
-        <h2>Iniciar Sesión</h2>
-
+        <h2>Crear Cuenta</h2>
+        <Form.Group controlId="formUsername">
+          <Form.Control
+            required
+            type="username"
+            placeholder="Usuario"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="form-control"
+          />
+          <Form.Control.Feedback type="invalid">
+            Ingresá un usuario válido.
+          </Form.Control.Feedback>
+        </Form.Group>
+        
         <Form.Group controlId="formEmail">
           <Form.Control
             required
@@ -67,14 +79,36 @@ export const Login = () => {
           </Form.Control.Feedback>
         </Form.Group>
 
-        {error && <p className="login-error">{error}</p>}
+       <Form.Group controlId="formRole">
+        <Form.Select
+            required
+            className="form-control"
+            value={career}
+            onChange={(e) => setCareer(e.target.value)}
+  >
+            <option value="" disabled>
+                Seleccioná tu carrera
+            </option>
+            <option value="civil">Ingeniería Civil</option>
+            <option value="electrica">Ingeniería Eléctrica</option>
+            <option value="industrial">Ingeniería Industrial</option>
+            <option value="mecanica">Ingeniería Mecánica</option>
+            <option value="quimica">Ingeniería Química</option>
+            <option value="sistemas">Ingeniería en Sistemas</option>
+        </Form.Select>
+        <Form.Control.Feedback type="invalid">
+            Seleccioná una carrera.
+        </Form.Control.Feedback>
+        </Form.Group>
+
+        {error && <p className="signup-error">{error}</p>}
 
         <Button type="submit" className="w-100">
-          Ingresar
+          Registrarme
         </Button>
 
-        <p className="login-register-link">
-          ¿No tenés cuenta? <Link to="/signup">Registrate</Link>
+        <p className="signup-register-link">
+          ¿Ya tenés cuenta? <Link to="/">Inicia Sesión</Link>
         </p>
       </Form>
     </div>
