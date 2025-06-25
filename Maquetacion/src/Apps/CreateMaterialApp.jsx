69a74a85735a9ca4b761f0256c6946548f84d5a7
nicodiezz <nicolasagusdiez@gmail.com>
2025-06-25 20:04:35 -0300

    Agrego useForm a createMaterialApp.jsx

--- Maquetacion/src/Apps/CreateMaterialApp.jsx
+++ Maquetacion/src/Apps/CreateMaterialApp.jsx
@@ -1,38 +1,41 @@
-import { useState } from 'react';
+
 import { Form, Row, Col, Button } from 'react-bootstrap';
 import { SearchOptions } from '../Components/SearchOptions.jsx';
 import { carreras } from './HomeApp.jsx';
+import { useForm } from '../Hooks/useForm.jsx';
 const materias = ["A1", "A2", "B1", "B2", "C1", "C2", "D1", "D2", "E1", "E2", "F1", "F2", "G1", "G2", "H1", "H2"];
 
 export const CreateMaterialApp = () => {
+  const [formData,setFormData,handleChange] = useForm()
+  
   return (
     <>
       <Form className="container mt-5 bg-light p-4 rounded">
         <h1 className="text-center mb-4">Subí tu material de estudio</h1>
         <Form.Group className="mb-5" aria-required>
           <Form.Label className='text-center w-100'>Ponele título</Form.Label>
-          <Form.Control name="titulo" size="lg" type="text" placeholder="Ej: Resumen Ecuaciones Diferenciales" />
+          <Form.Control onChange={handleChange} name="titulo" size="lg" type="text" placeholder="Ej: Resumen Ecuaciones Diferenciales" />
         </Form.Group>
         <Form.Group controlId="formFileMultiple" className="mb-5" aria-required>
           <Form.Label>Subí los archivos que quieras publicar</Form.Label>
-          <Form.Control type="file" name="archivos" multiple />
+          <Form.Control onChange={handleChange} type="file" name="archivos" multiple />
         </Form.Group>
         <Row className="mb-3">
           <Col>
             <Form.Group aria-required>
-              <SearchOptions options={materias} name="materia" placeholder="De que materia es?" />
+              <SearchOptions options={materias} onChange={ handleChange } name="materia" placeholder="De que materia es?" />
             </Form.Group>
           </Col>
           <Col>
             <Form.Group aria-required>
-              <SearchOptions options={carreras.map((e) => e.name)} name="carrera" placeholder="En que carrera la cursaste?" />
+              <SearchOptions options={carreras.map((e) => e.name)} onChange={ handleChange } name="carrera" placeholder="En que carrera la cursaste?" />
             </Form.Group>
           </Col>
         </Row>
         <Row className="mb-3">
           <Col>
             <Form.Group >
-              <Form.Select name="tipo">
+              <Form.Select onChange={handleChange} name="tipo">
                 <option>Tipo de material</option>
                 <option value="parcial">Parcial</option>
                 <option value="parcial resuelto">Parcial Resuelto</option>
@@ -48,7 +51,7 @@ export const CreateMaterialApp = () => {
           </Col>
           <Col>
             <Form.Group  >
-              <Form.Select name="parcial">
+              <Form.Select onChange={handleChange} name="parcial">
                 <option>Parcial con el que se relaciona</option>
                 <option value={0}>Ninguno</option>
                 <option value={1}>1ero</option>
@@ -60,13 +63,13 @@ export const CreateMaterialApp = () => {
           </Col>
           <Col>
             <Form.Group >
-              <Form.Control type="text" placeholder="Comisión" name="comision"></Form.Control>
+              <Form.Control onChange={handleChange} type="text" placeholder="Comisión" name="comision"></Form.Control>
             </Form.Group>
           </Col>
         </Row>
         <Form.Group className="mb-3">
           <Form.Label>Descripción del material</Form.Label>
-          <Form.Control name="descripcion" as="textarea" rows={3} placeholder="Escribí una breve descripción del material que estás subiendo." />
+          <Form.Control onChange={handleChange} name="descripcion" as="textarea" rows={3} placeholder="Escribí una breve descripción del material que estás subiendo." />
         </Form.Group>
           <Button type="submit" className="d-block mx-auto" variant="primary">
             Subir
