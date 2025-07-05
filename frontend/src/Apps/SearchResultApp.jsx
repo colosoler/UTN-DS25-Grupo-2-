5027d3a4cb7261ee5a452ae0d851cbd4bc281768
nicodiezz <nicolasagusdiez@gmail.com>
2025-07-05 16:08:47 -0300

    obtengo los parametros de la busqueda en searchResultApp

--- frontend/src/Apps/SearchResultApp.jsx
+++ frontend/src/Apps/SearchResultApp.jsx
@@ -2,21 +2,24 @@ import { Searchbar } from '../Components/Searchbar.jsx'
 import { Result } from './../Components/Result.jsx';
 import { useFetch } from '../Hooks/useFetch.jsx';
 import { SERVER_URL } from '../Constants.js';
+import { useSearchParams } from 'react-router-dom';
 import './styles/SearchResultApp.css';
 
 export const SearchResultApp = () => {
+  const [searchParams] = useSearchParams();
   const { data, isLoading, error } = useFetch("/apuntes", SERVER_URL);
   if (isLoading) return <p>Cargando apuntes...</p>;
   if (error) return <p>Error: {error.message}</p>;
-
+  
   const getFilteredResults = () => {
-    const carrera = data.get('carrera');
-    const materia = data.get('materia');
-    const tipo = data.get('tipo');
-    const parcial = data.get('parcial');
-    const comision = data.get('comision');
-    //filtrar
-    return data 
+    const carrera = searchParams.get('carrera');
+    const materia = searchParams.get('materia');
+    const tipo = searchParams.get('tipo');
+    const parcial = searchParams.get('parcial');
+    const comision = searchParams.get('comision');
+    //filtrar en el back
+    let filteredResults = data;
+    return filteredResults 
   }
 
     return (
@@ -24,7 +27,7 @@ export const SearchResultApp = () => {
         <Searchbar></Searchbar>
         <section id='results'>
           <div className="results-list">
-            {data.map(result => (
+            {getFilteredResults().map(result => (
               <Result key={result.id} result={result} shareUrl={`${window.location.origin}/material/${result.id}`} />
             ))}
           </div>
