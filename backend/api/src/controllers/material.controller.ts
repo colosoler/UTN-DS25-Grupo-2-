fa6f75b5a08acc4cfc0296d6a4dcffaa8645a31e
andradasantiago1 <andradasantiago.004@gmail.com>
2025-08-05 22:08:34 -0300

    correccion del material controller

--- backend/api/src/controllers/material.controller.ts
+++ backend/api/src/controllers/material.controller.ts
@@ -3,12 +3,34 @@ import { Request, Response, NextFunction } from 'express';
 import { Materials, CreateMaterialsRequest, UpdateMaterialsRequest } from '../types/materials.types';
 import * as materialService from '../services/materials.service';
 
-export async function getMaterials(req: Request, res:Response<MaterialsListResponse>, next: NextFunction) {
+export async function getAllMaterials(req: Request, res:Response<MaterialsListResponse>, next: NextFunction) {
 	try {
 		const searchParams = req.query;
+		const materials = await materialService.getAllMaterials(searchParams);
+		res.json({
+			materials,
+		});
+	} catch (error) {
+		next(error);
+	}
+}
+
+export async function getMaterialsByFilters(req: Request, res: Response<MaterialsListResponse>, next: NextFunction) {
+	try {
+		const searchParams = req.query as {
+            query?: string;
+            materia?: string;
+            carrera?: string;
+            comision?: string;
+            parcial?: string;
+            tipo?: string;
+            user?: string;
+        };
 		const materials = await materialService.findMaterials(searchParams);
 		res.json({
 			materials,
+			total: materials.length,
+			message: 'Materiales filtrados recuperados exitosamente'
 		});
 	} catch (error) {
 		next(error);
@@ -24,19 +46,6 @@ export async function createMaterial( req: Request<{}, MaterialResponse,CreateMa
         });
     } catch (error) {
     next(error);
-    }
-}
-
-export async function getMaterialById(req: Request, res: Response<MaterialResponse>, next: NextFunction) {
-    try {
-        const { id } = req.params;
-        const material = await materialService.getMaterialById(parseInt(id));
-        res.json({
-            material,
-            message: 'Material recuperado con exito'
-        });
-    } catch (error) {
-        next(error);
     }
 }
 
@@ -57,19 +66,6 @@ export async function updateMaterial(req: Request<{ id: string }, MaterialRespon
         res.json({
             material: updatedMaterial,
             message: 'Material actualizado exitosamente'
-        });
-    } catch (error) {
-        next(error);
-    }
-}
-
-export async function getMaterialsByCourseAndSubject(req: Request, res: Response, next: NextFunction) {
-    try {
-        const { idMateria, idCarrera } = req.params; //asumo q los parametros son unicamente estos 2.
-        const materiales = await materialService.getMaterialsByCourseAndSubject(idMateria, idCarrera);
-        res.json({
-            materials,
-            message: 'Materiales recuperados con exito'
         });
     } catch (error) {
         next(error);
