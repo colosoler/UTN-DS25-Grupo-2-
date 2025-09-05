d90122d2e46edc4e7ece467ed8977fce418123cd
Franco Arce <165492758+FrancoArcee@users.noreply.github.com>
2025-09-05 10:13:08 -0300

    Cambio estética de la navbar

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,9 +1,9 @@
 import './styles/Navbar.css';
 import { Link, useNavigate } from 'react-router-dom';
-import { Dropdown } from 'react-bootstrap';
+import { Navbar as RBNavbar, Nav, Dropdown, Container } from 'react-bootstrap';
 import { useState } from 'react';
 import { DeleteConfirmAccount } from '../Components/DeleteConfirmAccount';
-import { Alert } from '../Components/Alert'; 
+import { Alert } from '../Components/Alert';
 
 export const Navbar = () => {
   const [showLogoutModal, setShowLogoutModal] = useState(false);
@@ -17,9 +17,6 @@ export const Navbar = () => {
     setShowLogoutModal(false);
     setShowLogoutToast(true);
 
-    // Simulás el cierre de sesión (limpiar token, etc) acá si corresponde
-
-    // Después de 3 segundos (mismo timeout que el toast) navegás a login o home
     setTimeout(() => {
       navigate('/');
       setShowLogoutToast(false);
@@ -32,41 +29,50 @@ export const Navbar = () => {
 
   return (
     <>
-      <nav className="navbar">
-        <div className="navbar-logo">
-          <span>📝<strong>UTN</strong>otas</span>
-        </div>
-        <div className="navbar-icons">
-          <Link to="/home"><img src="./images/hogar.png" alt="Inicio" /></Link>
-          <Link to="/add"><img src="./images/plus-pequeno.png" alt="Agregar contenido" /></Link>
-          <Dropdown align="end">
-            <Dropdown.Toggle as="div" className="avatar-toggle">
-              <img src="./images/avatar.png" alt="Perfil" className="avatar-img" />
-            </Dropdown.Toggle>
+      <RBNavbar expand="lg" className="navbar">
+        <Container fluid className="d-flex justify-content-between align-items-center">
+          {/* Logo + título como Link */}
+          <RBNavbar.Brand as={Link} to="/home" className="navbar-logo">
+            <img src="/images/UTNotas.png" alt="UTNotas Logo" className="logo-img" />
+            <span><strong>UTN</strong>otas</span>
+          </RBNavbar.Brand>
 
-            <Dropdown.Menu className='dropdown-menu'>
-              <Dropdown.Item className='dropdown-item' as={Link} to="/profile">
-                <img className='dropdown-icon' src="./images/user-icon2.svg" alt="profile" />
-                Ver perfil
-              </Dropdown.Item>
-              <Dropdown.Item className='dropdown-item' as={Link} to="/myposts">
-                <img className='dropdown-icon' src="./images/files-icon.svg" alt="mis-publicaciones" />
-                Mis publicaciones
-              </Dropdown.Item>
-              <Dropdown.Item className='dropdown-item' as={Link} to="/settings">
-                <img className='dropdown-icon' src="./images/settings-icon.svg" alt="configurar" />
-                Configurar perfil
-              </Dropdown.Item>
-              <Dropdown.Divider />
-              <Dropdown.Item className='dropdown-item' onClick={handleOpenLogoutModal}>
-                <img className='dropdown-icon' src="./images/logout-icon.svg" alt="logout" />
-                Cerrar Sesión
-              </Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-        </div>
-      </nav>
+          {/* Íconos a la derecha */}
+          <Nav className="navbar-icons">
+            <Link to="/home"><img src="./images/hogar.png" alt="Inicio" /></Link>
+            <Link to="/add"><img src="./images/plus-pequeno.png" alt="Agregar contenido" /></Link>
+            
+            {/* Dropdown de perfil */}
+            <Dropdown align="end">
+              <Dropdown.Toggle as="div" className="avatar-toggle">
+                <img src="./images/avatar.png" alt="Perfil" className="avatar-img" />
+              </Dropdown.Toggle>
 
+              <Dropdown.Menu className="dropdown-menu">
+                <Dropdown.Item className="dropdown-item" as={Link} to="/profile">
+                  <img className="dropdown-icon" src="./images/user-icon2.svg" alt="profile" />
+                  Ver perfil
+                </Dropdown.Item>
+                <Dropdown.Item className="dropdown-item" as={Link} to="/myposts">
+                  <img className="dropdown-icon" src="./images/files-icon.svg" alt="mis-publicaciones" />
+                  Mis publicaciones
+                </Dropdown.Item>
+                <Dropdown.Item className="dropdown-item" as={Link} to="/settings">
+                  <img className="dropdown-icon" src="./images/settings-icon.svg" alt="configurar" />
+                  Configurar perfil
+                </Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Item className="dropdown-item" onClick={handleOpenLogoutModal}>
+                  <img className="dropdown-icon" src="./images/logout-icon.svg" alt="logout" />
+                  Cerrar Sesión
+                </Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Nav>
+        </Container>
+      </RBNavbar>
+
+      {/* Modal de logout */}
       <DeleteConfirmAccount
         show={showLogoutModal}
         onHide={handleCloseLogoutModal}
@@ -77,6 +83,7 @@ export const Navbar = () => {
         buttonTitle="Cerrar sesión"
       />
 
+      {/* Toast de confirmación */}
       <Alert
         show={showLogoutToast}
         message="Sesión cerrada correctamente"
