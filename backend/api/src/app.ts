941a11312d9098164537c153df9847fa4c83257f
Dante Manuel Barbé Lorenzo <dante_0312@hotmail.com>
2025-09-10 13:04:47 -0300

    modifico app.ts

--- backend/api/src/app.ts
+++ backend/api/src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { userRoutes } from './routes/user.routes';
+import { authRoutes} from './routes/auth.routes'
 import { materialRoutes } from './routes/material.routes';
 import { materiaRoutes } from './routes/materia.route'
 import { handleError } from './middlewares/error.middleware';
@@ -15,10 +16,15 @@ app.use(logRequest);
 
 const cors = require('cors');
 var corsOptions = {
-  origin: process.env.FRONTEND_URL
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }
+
 app.use(cors(corsOptions));
 
+app.use('/auth', authRoutes);
 app.use('/users', userRoutes);
 app.use('/materials', materialRoutes);
 app.use('/materias', materiaRoutes);
