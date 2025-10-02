60db3e4e49351a80f9f252e51ddbbe29c388871b
nicodiezz <nicolasagusdiez@gmail.com>
2025-10-02 16:30:37 -0300

    Creo endpoint /signup/hash/ para hashear contraseñas y crear admins desde prisma studio

--- backend/api/src/routes/signup.routes.ts
+++ backend/api/src/routes/signup.routes.ts
@@ -7,4 +7,13 @@ const router = Router();
 
 router.post('/', validate(createUserSchema), signup);
 
+import bcrypt from 'bcrypt';
+router.post('/hash',
+    async (req,res,next) => {
+        let hash = await bcrypt.hash(req.body.password, 10);
+        console.log(hash);
+        res.json(hash);
+        next()
+    })
+
 export const signupRoutes = router;
