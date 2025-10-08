77d50a8db56af3d396d52a48d22c53ceeee0d291
Ramiro Gil <gilramiro49@gmail.com>
2025-10-08 18:49:32 -0300

    Modifico ProfilePage

--- frontend/src/Apps/ProfilePage.jsx
+++ frontend/src/Apps/ProfilePage.jsx
@@ -1,6 +1,26 @@
+import { getUser, getToken } from "../Helpers/auth";
+import { useEffect, useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
 import './styles/ProfilePage.css';
 
 export const ProfilePage = () => {
+  const navigate = useNavigate();
+  const [UserData, setUserData] = useState(null);
+  const user = getUser();
+
+  useEffect(() => {
+    if (!user) return;
+
+    const UserData = fetch(`http://localhost:3000/users/${user.id}`, {
+      headers: { "Authorization": `Bearer ${getToken()}` },
+    })
+      .then(res => res.json())
+      .then(data => setUserData(data))
+      .catch(err => console.error(err));
+  }, [user]);
+
+  if (!UserData) return <p>Cargando...</p>;
+
   return (
     <div className="profile-container">
       <div className="profile-card">
@@ -9,9 +29,13 @@ export const ProfilePage = () => {
         </div>
 
         <div className="profile-info">
-          <h1 className="profile-name">Nombre y Apellido</h1>
-          <p className="profile-username">@nombreusuario</p>
-          <p className="profile-career">Estudiante de NombreCarrera</p>
+          <h1 className="profile-name">{UserData.name} {UserData.surname}</h1>
+          <p className="profile-username">@{UserData.username}</p>
+          <p className="profile-career">Estudiante de {UserData.career.nombre}</p>
+          <p className="profile-date">
+            <img src="../images/calendar.png" alt="" />
+            Se unió en {new Date(UserData.createdAt).toLocaleDateString('es-AR', { month: 'long', year: 'numeric' })}
+          </p>
 
           <div className="stats-container">
             <div className="stat-item positive">
