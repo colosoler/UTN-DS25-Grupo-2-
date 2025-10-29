defe85be10f30eadb539c7bb48d8b3bb9727210a
Dante Manuel Barbé Lorenzo <dante_0312@hotmail.com>
2025-10-29 14:25:33 -0300

    agrego los respectivos archivos de reporte

---
+++ backend/api/src/routes/reporte.routes.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+import * as reporteController from '../controllers/reporte.controller';
+import { validate } from '../middlewares/validation.middleware';
+import { updateReporteSchema } from
+    '../validations/reporte.validation';
+import { authenticate } from '../middlewares/auth.middleware';
+
+const router = Router();
+
+router.get('/:materialId/reportes', authenticate, reporteController.findReportes);
+
+router.get('/:materialId/reporte', authenticate, reporteController.getReporteByMaterialAndUser);
+
+router.post('/:materialId/reporte', authenticate,
+    reporteController.createReporteByMaterialAndUser);
+
+router.put('/:materialId/reporte', authenticate, validate(updateReporteSchema),
+    reporteController.updateReporteByMaterialAndUser);
+    
+router.delete('/:materialId/reporte', authenticate, reporteController.deleteReporteByMaterialAndUser);
+
+export const reporteRoutes = router;
