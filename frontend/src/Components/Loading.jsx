d82ce1117622c7d020e1828d8fba6d178439d5fc
Dante Manuel Barbé Lorenzo <dante_0312@hotmail.com>
2025-11-03 13:21:37 -0300

    arreglo CarreraDetailPage

--- frontend/src/Components/Loading.jsx
+++ frontend/src/Components/Loading.jsx
@@ -3,8 +3,10 @@ import { Spinner } from 'react-bootstrap';
 
 export const Loading = () => {
   return (
-    <div className="spinner-border text-primary" role="status">
-        <span>UTNotas...</span>
+    <div className="d-flex justify-content-center align-items-center vh-100">
+      <div className="spinner-border text-primary" role="status">
+          <span>UTNotas...</span>
+      </div>
     </div>
   )
 }
