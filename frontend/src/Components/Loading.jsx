be4ac7a34fd62f259bdfe3d6a78495171482e511
Ramiro Gil <gilramiro49@gmail.com>
2025-11-07 16:15:15 -0300

    Modifico componente Loading

--- frontend/src/Components/Loading.jsx
+++ frontend/src/Components/Loading.jsx
@@ -3,10 +3,9 @@ import { Spinner } from 'react-bootstrap';
 
 export const Loading = () => {
   return (
-    <div className="d-flex justify-content-center align-items-center vh-100">
-      <div className="spinner-border text-primary" role="status">
-          <span>UTNotas...</span>
-      </div>
+    <div className="d-flex flex-column justify-content-center align-items-center vh-100">
+      <Spinner animation="border" role="status" className="mb-3" />
+      <h4>Cargando...</h4>
     </div>
   )
 }
