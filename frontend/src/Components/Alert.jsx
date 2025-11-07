6a4aebddcfc47c81ee3a4c90c535f15fb39efc8f
andradasantiago1 <andradasantiago.004@gmail.com>
2025-11-06 22:56:34 -0300

    modifico estetica de Alert.jsx

--- frontend/src/Components/Alert.jsx
+++ frontend/src/Components/Alert.jsx
@@ -1,8 +1,10 @@
 import { useState, useEffect } from "react"
 import { Toast, ToastContainer } from "react-bootstrap"
+import "./styles/Alert.css";
 
 export const Alert = ({ show, message, onClose, variant = "success"}) => {
   const [showToast, setShowToast] = useState(show)
+  const color = "#4862caff";
 
   useEffect(() => {
     setShowToast(show)
@@ -22,29 +24,27 @@ export const Alert = ({ show, message, onClose, variant = "success"}) => {
   }
 
   return (
-    <ToastContainer position="top-center" className="p-3">
+    <ToastContainer position="top-center" className="p-3 m-2 m-lg-3 w-auto alert-max-width-md" style={{ zIndex: 1080 }}>
       <Toast
         show={showToast}
         onClose={handleClose}
-        style={{
-          backgroundColor: "var(--primary-color)",
-          color: "#ffffff",
-          border: "none",
-          borderRadius: "8px",
-          boxShadow: "0 4px 12px rgba(74, 144, 226, 0.3)",
-        }}
+        className="alert-custom-toast" 
+				style={{
+					backgroundColor: color,
+					color: "#ffffff",
+				}}
       >
         <Toast.Header
           closeButton={true}
-          style={{
-            backgroundColor: "var(--primary-color)",
-            color: "#ffffff",
-            border: "none",
-          }}
+          className="alert-custom-header" 
+					style={{
+						backgroundColor: color,
+						color: "#ffffff",
+					}}
         >
           <strong className="me-auto">UTNotas</strong>
         </Toast.Header>
-        <Toast.Body style={{ color: "#ffffff" }}>{message}</Toast.Body>
+        <Toast.Body className="alert-custom-body" style={{ color: "#ffffff" }}>{message}</Toast.Body>
       </Toast>
     </ToastContainer>
   )
