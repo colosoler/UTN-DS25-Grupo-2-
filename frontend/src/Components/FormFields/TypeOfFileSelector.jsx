48d5b9c8951cad1949562fe60ba3c86c917c8067
nicodiezz <nicolasagusdiez@gmail.com>
2026-03-31 18:51:33 -0300

    Agrego animación

--- frontend/src/Components/FormFields/TypeOfFileSelector.jsx
+++ frontend/src/Components/FormFields/TypeOfFileSelector.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { ExpandedSelector } from "./ExpandedSelector";
 import { Form } from "react-bootstrap";
 
-export const TypeOfFileSelector = ({useForm, fieldError}) => {
+export const TypeOfFileSelector = ({ useForm, fieldError }) => {
   const [type, setType] = useState()
   const [formData, setFormData, handleChange] = useForm;
   const FieldError = fieldError;
@@ -15,10 +15,11 @@ export const TypeOfFileSelector = ({useForm, fieldError}) => {
         ]}
         name="type_of_file"
         label="Qué querés publicar?"
-        onChange={(e) => { setType(JSON.parse(e.target.value).id); console.log(formData) }}
+        onChange={(e) => { setType(JSON.parse(e.target.value).id); }}
+        value={type}
       />
       {type === "PDF" &&
-        <Form.Group className="material-form-group">
+        <Form.Group className="material-form-group slide-down-form">
           <div className="d-flex justify-content-between align-items-baseline">
             <Form.Label className="material-form-label">Subí el archivo que quieras publicar</Form.Label>
             <FieldError field="archivos" />
@@ -27,13 +28,13 @@ export const TypeOfFileSelector = ({useForm, fieldError}) => {
             type="file"
             name="archivos"
             accept=".pdf"
-            onChange={(e) => setFormData({...formData, archivos: e.target.files})}
+            onChange={(e) => setFormData({ ...formData, archivos: e.target.files })}
             className="material-form-control"
             multiple
           />
         </Form.Group>
       }
-      {type === "Images" && <div>Subí una imágen</div>
+      {type === "Images" && <div className="slide-down-form">Subí una imágen</div>
       }
     </>
   )
